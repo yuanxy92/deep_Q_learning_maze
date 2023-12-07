034f52145e081562386bee3777c4c35c2ae5fe20
yuanxy92 <yuanxy92@gmail.com>
2023-12-07 03:31:44 +0000

    add

---
+++ train_dqn_multi.py
@@ -0,0 +1,238 @@
+import numpy as np
+import os
+import scipy.special as sp
+
+from IPython.display import display, clear_output
+import matplotlib.pyplot as plt
+import copy
+import time
+import random
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import collections
+
+from environment import MazeEnvironment
+
+Transition = collections.namedtuple('Experience',
+                                    field_names=['state', 'action',
+                                                 'next_state', 'reward',
+                                                 'is_game_on'])
+
+class ExperienceReplay:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = collections.deque(maxlen=capacity)
+
+    def __len__(self):
+        return len(self.memory)
+
+    def push(self, transition):
+        self.memory.append(transition)
+
+    def sample(self, batch_size, device = 'cuda'):
+        indices = np.random.choice(len(self.memory), batch_size, replace = False)
+        
+        states, actions, next_states, rewards, isgameon = zip(*[self.memory[idx] 
+                                                                for idx in indices])
+        
+        return torch.Tensor(states).type(torch.float).to(device), \
+               torch.Tensor(actions).type(torch.long).to(device), \
+               torch.Tensor(next_states).to(device), \
+               torch.Tensor(rewards).to(device), torch.tensor(isgameon).to(device)
+    
+class fc_nn(nn.Module):
+    def __init__(self, Ni, Nh1, Nh2, No = 4):
+        super().__init__()
+        
+        self.fc1 = nn.Linear(Ni, Nh1)
+        self.fc2 = nn.Linear(Nh1, Nh2)
+        self.fc3 = nn.Linear(Nh2, No)
+        
+        self.act = nn.ReLU()
+        
+    def forward(self, x, classification = False, additional_out=False):
+        x = self.act(self.fc1(x))
+        x = self.act(self.fc2(x))
+        out = self.fc3(x)
+        
+        return out
+    
+class conv_nn(nn.Module):
+    
+    channels = [16, 32, 64]
+    kernels = [3, 3, 3]
+    strides = [1, 1, 1]
+    in_channels = 1
+    
+    def __init__(self, rows, cols, n_act):
+        super().__init__()
+        self.rows = rows
+        self.cols = cols
+
+        self.conv = nn.Sequential(nn.Conv2d(in_channels = self.in_channels,
+                                            out_channels = self.channels[0],
+                                            kernel_size = self.kernels[0],
+                                            stride = self.strides[0]),
+                                  nn.ReLU(),
+                                  nn.Conv2d(in_channels = self.channels[0],
+                                            out_channels = self.channels[1],
+                                            kernel_size = self.kernels[1],
+                                            stride = self.strides[1]),
+                                  nn.ReLU()
+                                 )
+        
+        size_out_conv = self.get_conv_size(rows, cols)
+        
+        self.linear = nn.Sequential(nn.Linear(size_out_conv, rows*cols*2),
+                                    nn.ReLU(),
+                                    nn.Linear(rows*cols*2, int(rows*cols/2)),
+                                    nn.ReLU(),
+                                    nn.Linear(int(rows*cols/2), n_act),
+                                   )
+
+    def forward(self, x):
+        x = x.view(len(x), self.in_channels, self.rows, self.cols)
+        out_conv = self.conv(x).view(len(x),-1)
+        out_lin = self.linear(out_conv)
+        return out_lin
+    
+    def get_conv_size(self, x, y):
+        out_conv = self.conv(torch.zeros(1,self.in_channels, x, y))
+        return int(np.prod(out_conv.size()))
+    
+def Qloss(batch, net, gamma=0.99, device="cuda"):
+    states, actions, next_states, rewards, _ = batch
+    lbatch = len(states)
+    state_action_values = net(states.view(lbatch,-1))
+    state_action_values = state_action_values.gather(1, actions.unsqueeze(-1))
+    state_action_values = state_action_values.squeeze(-1)
+    
+    next_state_values = net(next_states.view(lbatch, -1))
+    next_state_values = next_state_values.max(1)[0]
+    
+    next_state_values = next_state_values.detach()
+    expected_state_action_values = next_state_values * gamma + rewards
+    
+    return nn.MSELoss()(state_action_values, expected_state_action_values)
+
+
+output_dir = './results/1'
+os.makedirs(output_dir, exist_ok=True)
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+maze = np.load('maze_generator/maze.npy')
+
+initial_position = [0,0]
+goal = [len(maze)-1, len(maze)-1]
+maze_env = MazeEnvironment(maze, initial_position, goal)
+
+maze_env.draw(f'{output_dir}/maze_20.pdf')
+buffer_capacity = 10000
+buffer_start_size = 1000
+memory_buffer = ExperienceReplay(buffer_capacity)
+
+from agent import Agent
+agent = Agent(maze = maze_env,
+              memory_buffer = memory_buffer,
+              use_softmax = True
+             )
+
+net = fc_nn(maze.size, maze.size, maze.size, 4)
+optimizer = optim.Adam(net.parameters(), lr=1e-4)
+
+device = 'cuda'
+batch_size = 24
+gamma = 0.9
+
+net.to(device)
+
+num_epochs = 20000
+cutoff = 3000
+epsilon = np.exp(-np.arange(num_epochs)/(cutoff))
+epsilon[epsilon > epsilon[100*int(num_epochs/cutoff)]] = epsilon[100*int(num_epochs/cutoff)]
+plt.plot(epsilon, color = 'orangered', ls = '--')
+plt.xlabel('Epochs')
+plt.ylabel('Epsilon')
+plt.savefig(f'{output_dir}/epsilon_profile.pdf', dpi = 300, bbox_inches = 'tight')
+plt.show()
+
+mp = []
+mpm = []
+reg = 200
+for e in epsilon:
+    a = agent.env.reset_policy(e)
+    mp.append(np.min(a))
+    mpm.append(np.max(a))
+
+plt.plot(epsilon/1.3, color = 'orangered', ls = '--', alpha = 0.5,
+         label= 'Epsilon profile (arbitrary units)')
+
+plt.plot(np.array(mpm)-np.array(mp), label = 'Probability difference', color = 'cornflowerblue')
+plt.xlabel('Epochs')
+plt.ylabel(r'max $p^r$ - min $p^r$')
+plt.legend()
+plt.savefig(f'{output_dir}/reset_policy.pdf', dpi = 300, bbox_inches = 'tight')
+plt.show()
+
+loss_log = []
+best_loss = 1e5
+
+running_loss = 0
+clear_line_num = 4
+for lineidx in range(clear_line_num):
+    print('#')
+
+for epoch in range(num_epochs):
+    loss = 0
+    counter = 0
+    eps = epsilon[epoch]
+    
+    agent.isgameon = True
+    _ = agent.env.reset(eps)
+    
+    while agent.isgameon:
+        agent.make_a_move(net, eps)
+        counter += 1
+        
+        if len(agent.buffer) < buffer_start_size:
+            continue
+            
+        optimizer.zero_grad()
+        batch = agent.buffer.sample(batch_size, device = device)
+        loss_t = Qloss(batch, net, gamma = gamma, device = device)
+        loss_t.backward()
+        optimizer.step()
+        
+        loss += loss_t.item()
+    
+    if (agent.env.current_position == agent.env.goal).all():
+        result = 'won'
+    else:
+        result = 'lost'
+    
+    if epoch%1000 == 0:
+        agent.plot_policy_map(net, f'{output_dir}/sol_epoch_'+str(epoch)+'.pdf', [0.35,-0.3])
+    
+    loss_log.append(loss)
+    
+    if (epoch > 2000):
+        running_loss = np.mean(loss_log[-50:])
+        if running_loss < best_loss:
+            best_loss = running_loss
+            torch.save(net.state_dict(), f'{output_dir}/best.torch')
+            estop = epoch
+    
+    for lineidx in range(clear_line_num):
+        print("\033[A\033[2K", end="\r")
+    print('Epoch', epoch, '(number of moves ' + str(counter) + ')')
+    print('Game', result)
+    print('[' + '#'*(100-int(100*(1 - epoch/num_epochs))) +
+          ' '*int(100*(1 - epoch/num_epochs)) + ']')
+    print('\t Average loss: ' + f'{loss:.5f}')
+    if (epoch > 2000):
+        print('\t Best average loss of the last 50 epochs: ' + f'{best_loss:.5f}' + ', achieved at epoch', estop)
+        clear_line_num = 5
+    else:
+        clear_line_num = 4
+    
